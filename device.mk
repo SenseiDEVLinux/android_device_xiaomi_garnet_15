f1225af4891606f5abb23fcc1b7802c5beb0d10a
Cristian Vázquez <cristianvaz81993@gmail.com>
2022-03-04 08:58:59 +0100

    garnet: Build missing Bluetooth A2DP
    I hwservicemanager: getTransport: Cannot find entry android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default in either framework or device VINTF manifest.
    
    E LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default.
    
    W audiohalservice: Could not register Bluetooth Audio Offload API

--- device.mk
+++ device.mk
@@ -119,6 +119,18 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# A2DP
+PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-service
+ 
+ PRODUCT_COPY_FILES += \
+     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml
+
+>>>>>>> ca6018e (garnet: Build missing Bluetooth A2DP)
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default:64 \
