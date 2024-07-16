8ed2ee6e5b3cced330eec78efcaf78a1a55ce70c
LuK1337 <priv.luk@gmail.com>
2024-07-17 01:51:53 +0530

    garnet: Partially import parts from sm8350-common
    Added:
    
    * Thermals
    * Refresh rate
    * Clear speaker
    * Saturation Slider
    
    Change-Id: 6b40e6a2aaf53e3a7438bc44b840f16362jdb861
    Signed-off-by: kenway214 <kenway214@outlook.com>
    
    sm8450-common: parts: Rename xiaomiparts title to Xiaomi Settings
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>
    
    peridot: Introduce displayfeature color service and use MIUI color modes
    
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I7faa409bf7069108f5bed64488b2a7c5566cd921
    
    peridot: Link displayfeature binder to death
    
    If displayfeature HAL crashes and restarts, we must recreate our
    IDisplayFeature instance.
    
    Change-Id: Icefc248bda251126f66204e4f1ced2ae4a1601c6
    
    peridot: migrate to dislayfeature AIDL
    
    peridot: Introduce displayfeature color service and use MIUI color modes
    
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I7faa409bf7069108f5bed64488b2a7c5566cd921
    
    peridot: Link displayfeature binder to death
    
    If displayfeature HAL crashes and restarts, we must recreate our
    IDisplayFeature instance.
    
    Change-Id: Icefc248bda251126f66204e4f1ced2ae4a1601c6
    
    peridot: migrate to dislayfeature AIDL
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>
    Co-Authored-By: lolipuru <banstellazz@gmail.com>
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-Authored-By: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-Authored-By: nullxception <enuma.alrizky@gmail.com>
    Co-Authored-By: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-Authored-By: TheScarastic <warabhishek@gmail.com>
    Co-Authored-By: kenway <102547769+kenway214@users.noreply.github.com>
    Co-Authored-By: Adarsh Grewal <adarshgrewal@gmail.com>
    Co-Authored-By: Raghav <63952537+raghavt20@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -562,6 +562,10 @@ PRODUCT_PACKAGES += \
     libinput_shim \
     libgui_shim
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
