3c1cbc82fc24273a077331e8ca605aceb2a416ad
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-21 22:44:16 +0300

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+rm -rf hardware/qcom-caf/sm8550
+rm -rf hardware/qcom-caf/sm8150
+rm -rf hardware/qcom-caf/sm8650
+rm -rf hardware/qcom-caf/msm8996
+rm -rf hardware/qcom-caf/msm8998
+rm -rf hardware/qcom-caf/sdm660
+rm -rf hardware/qcom-caf/sm8250
+rm -rf hardware/qcom-caf/sm8350
+rm -rf hardware/qcom-caf/msm8953
+rm -rf hardware/qcom-caf/sdm845
