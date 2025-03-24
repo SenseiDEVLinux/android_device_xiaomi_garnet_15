e1d6fd136059f70ad52a3739b20bd05245a7560a
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-24 23:28:30 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,6 @@ rm -rf hardware/qcom-caf/msm8996
 rm -rf hardware/qcom-caf/msm8953
 rm -rf hardware/qcom-caf/sdm845
 rm -rf hardware/qcom-caf/sdm660
+rm -rf hardware
+rm -rf device/qcom/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr device/qcom/sepolicy_vndr/sm8450
