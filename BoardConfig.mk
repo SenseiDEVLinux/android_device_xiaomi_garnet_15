54751b997abca3dfd5215325c7975841921a9a0f
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-24 14:27:13 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,6 +207,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # System properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
