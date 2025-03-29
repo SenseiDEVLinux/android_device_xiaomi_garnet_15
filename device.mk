ff64e21d1b5bae1927649b43a5786234ac7ff495
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-29 20:17:04 +0300

    garnet: Remove the keys for signing builds Signed-off-by: Sensei <senseidev23@gmail.com>

--- device.mk
+++ device.mk
@@ -593,7 +593,5 @@ PRODUCT_PACKAGES += \
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
--include vendor/lineage-priv/keys/keys.mk
-
 # ViperFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
